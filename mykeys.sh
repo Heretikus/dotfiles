1aa18f3ad9afcb953b20eda2654afb3f44c34aa7
Heretikus <heretikush@gmail.com>
2018-03-15 21:07:38 +0200

    gitconfigadd

---
+++ mykeys.sh
@@ -0,0 +1,15 @@
+IFS="$(printf '\n\t')"
+
+mkdir -p ~/.ssh
+
+if ! [[ -f ~/.ssh/authorized_keys ]]; then
+  echo "Creating new ~/.ssh/authorized_keys"
+  touch ~/.ssh/authorized_keys
+fi
+
+keys=`curl https://api.github.com/users/heretikus/keys | grep -o -E "ssh-\w+\s+[^\"]+"`
+
+for key in $keys; do
+  echo $key
+  grep -q "$key" ~/.ssh/authorized_keys || echo "$key" >> ~/.ssh/authorized_keys
+done
